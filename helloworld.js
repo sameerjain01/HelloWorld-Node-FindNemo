c5bc64f5de3bc764722d4a3573ca8ed63d85a998
Sameer Jain <55339515+sameerjain01@users.noreply.github.com>
2020-06-13 23:53:18 -0500

    Update helloworld.js
    minor updates for code and variable naming

--- helloworld.js
+++ helloworld.js
@@ -1,7 +1,7 @@
 const { performance } = require('perf_hooks'); //object destructuring
 
-
-console.log("Hello Javascript!");
+//Prine Hello World
+console.log("Hello World of Javascript!");
 
 function printName() {
     let name;
@@ -13,22 +13,29 @@ function printName() {
 printName();
 
 
-const nemo = ['nemo'];
-const sub1 = new Array(450).fill('NOT-NEMO');
-const sub2 = new Array(450).fill('DefinitelyNOTNemo');
+const nemo = ['nemo']; //initialize nemo
 
-const finalsear = sub1.concat(nemo).concat(sub2);
-//const nemo = [];
+//initialize and fill a random array1
+const randomArray1 = new Array(450).fill('NOT-NEMO'); 
+
+//initialize and fill a random array2
+const randomArray2 = new Array(450).fill('DefinitelyNOTNemo');
+
+//cretae a final by concatenation of three arrays
+const finalArray = randomArray1.concat(nemo).concat(randomArray2);
+
 function findNemo(array) {
     let t0 = performance.now();
 
     if (array && array.length > 0) {
+        
         console.log("Total length of input array is " + array.length);
+        
         for (let idx in array) {
             let item = array[idx]
-            console.log("item at index #" + idx + " is " + item);
+            console.log("Item at index#" + idx + " is " + item);
             if (item === 'nemo') {
-                console.log('i found nemo at index #' + idx);
+                console.log('We found nemo at index #' + idx);
                                 break;
             }
 
@@ -36,7 +43,7 @@ function findNemo(array) {
 
     } else {
 
-        console.log('whooaa empty array');
+        console.log('whooaa!!! empty array');
     }
 
     let t1 = performance.now();
@@ -44,4 +51,4 @@ function findNemo(array) {
 
 }
 
-findNemo(finalsear);
+findNemo(finalArray);
